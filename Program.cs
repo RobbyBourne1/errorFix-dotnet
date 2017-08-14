c0350977cadc0252625decd4d19772ec89c7c84a
Robby Bourne <robbybourne1@gmail.com>
2017-08-14 13:48:58 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿public class School {public static void main(String[] args) {
+        int numStudents;
+        int NumberOfTeachersInTheSchool;
+        double average_gpa;
+
+
+
+
+
+
+        String n;
+        String CityInWhichTheSchoolResides;
+        boolean IS_PUBLIC;
+
+        numStudents = "403";
+    city = Atlanta, GA;
+    numTeachers = "62";
+    averageGPA = 2.98
+    name = Grady High School;
+        isPublic = yes;
+
+        println("School: " + name);
+        println("Teachers: " numberOfTeachers);
+
+        function(averageGPA, city);int var = null;
+
+        if (numStudents) {print "We have at least 10 students"}
+        while (numTeach) {
+            System.out.println(We have at least 5 teachers);
+        }
+    }
+
+public static function (double x, double y) {
+    System.out.println(x + ", ", + y.toString());
+}
+}
