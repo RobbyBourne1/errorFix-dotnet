b0f11ebbe03dca7d3f66f939d33c5e6a1cc5ddec
Robby Bourne <robbybourne1@gmail.com>
2017-08-14 14:22:55 -0400

    Got the program to compile

--- Program.cs
+++ Program.cs
@@ -1,43 +1,37 @@
 ﻿using System;
 
 namespace errorFix
-{ 
+{
     class Program
     {
- 
- public static void main(string[] args) {
-        int numStudents;
-        int NumberOfTeachersInTheSchool;
-        double average_gpa;
 
-        string n;
-        string CityInWhichTheSchoolResides;
-        bool IS_PUBLIC;
-
-        numStudents = 403;
-    var city = "Atlanta, GA";
-    var numTeachers = 62;
-    var averageGPA = 2.98;
-    var name = "Grady High School";
-    var isPublic = true;
-
-        Console.WriteLine("School: " + name);
-        Console.WriteLine("Teachers: ", numTeachers);
-
-        function(averageGPA, city);
-
-        if (numStudents >= 10) 
+        public static void Main(string[] args)
         {
-            Console.WriteLine("We have at least 10 students");
-        }
-        if (numTeachers >= 5) 
-        {
-            Console.WriteLine("We have at least 5 teachers");
-        }
-    }
+            int numStudents;
 
-public static void Main(double x, string y) {
-    Console.WriteLine(x + ", " + y);
+            numStudents = 403;
+            var numTeachers = 62;
+            var averageGPA = 2.98;
+            var name = "Grady High School";
+            Console.WriteLine("School: " + name);
+            Console.WriteLine("Teachers: ", numTeachers);
+
+            if (numStudents >= 10)
+            {
+                Console.WriteLine("We have at least 10 students");
+            }
+            if (numTeachers >= 5)
+            {
+                Console.WriteLine("We have at least 5 teachers");
+            }
+            function(averageGPA, name);
+            function(numStudents, averageGPA);
+        }
+        public static double function(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static void function(double averageGPA, string name) => Console.WriteLine($"The average GPA of students at {name} was {averageGPA}.");
     }
-    } 
 }
